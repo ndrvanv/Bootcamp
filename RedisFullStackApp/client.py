10ce7d1bc4a78ada570c6c98935a71b2ee0d98c6
Andrew <it.andr@outlook.com>
2022-11-01 19:21:41 +0300

    added client and server working with redis

---
+++ RedisFullStackApp/client.py
@@ -0,0 +1,7 @@
+from wsgiref import validate
+import redis
+
+with redis.Redis() as redis_client:
+    value = redis_client.brpop("queue")
+
+print(int(value[1]))
---
+++ RedisFullStackApp/server.py
@@ -0,0 +1,6 @@
+from calendar import c
+import redis
+import random
+
+with redis.Redis() as redis_server:
+    redis_server.lpush("queue", random.randint(0, 10))
